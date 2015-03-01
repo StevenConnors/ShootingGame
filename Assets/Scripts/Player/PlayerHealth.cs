9622de0e62c06e1557f3cf33000ffb2ed27674e0
Steven C. Connors <scconnor@andrew.cmu.edu>
2015-03-01 06:12:28 -0500

    our final submission to HackIllinois

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //need this to use the UI components
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100; //dictates initial health
+    public int currentHealth;
+    public Slider healthSlider; //reference to the slider UI element that we created
+    public Image damageImage;  //reference to damageimage we created
+    public AudioClip deathClip;
+    public float flashSpeed = 5f; //how quickly the damageImage flashes 
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f); //color of damageImage 
+    
+    
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    PlayerShooting playerShooting;
+    bool isDead;
+    bool damaged;
+    
+	public float restartDelay = 15f;
+	float restartTimer;   
+    
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();
+        playerAudio = GetComponent <AudioSource> ();
+        playerMovement = GetComponent <PlayerMovement> (); //use the name of the script 
+        playerShooting = GetComponentInChildren <PlayerShooting> ();
+        currentHealth = startingHealth;
+    }
+    
+    
+    void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+    
+    
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+        
+        currentHealth -= amount;
+        
+        healthSlider.value = currentHealth;
+        
+        playerAudio.Play ();
+        
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death ();
+        }
+    }
+    
+    
+    void Death ()
+    {
+        isDead = true;
+        
+        playerShooting.DisableEffects ();
+        
+        anim.SetTrigger ("Die");
+        
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+        
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }
+    
+    
+    public void RestartLevel ()
+    {
+	    // .. increment a timer to count up to restarting.
+		restartTimer += Time.deltaTime;
+		
+		// .. if it reaches the restart delay...
+		if(restartTimer >= restartDelay)
+		{
+			// .. then reload the currently loaded level.
+			Application.LoadLevel(Application.loadedLevel);
+		}
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+using Thalmic;
+
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+	// Myo game object to connect with.
+	// This object must have a ThalmicMyo script attached.
+	public GameObject myo = null;
+
+    float timer;
+    float myoTimer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+
+    int bulletCount = 0;
+    int count = 0;
+    float myoCountConstant = 5.0f;
+
+	private Pose _lastPose = Pose.Unknown;
+
+    void Awake ()
+    {
+        shootableMask = LayerMask.GetMask ("Shootable");
+        gunParticles = GetComponent<ParticleSystem> ();
+        gunLine = GetComponent <LineRenderer> ();
+        gunAudio = GetComponent<AudioSource> ();
+        gunLight = GetComponent<Light> ();
+    }
+	
+    void Update ()
+    {
+       	timer += Time.deltaTime;
+        myoTimer += Time.deltaTime;
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+    	if (thalmicMyo.pose == Pose.Fist && timer >= timeBetweenBullets && Time.timeScale != 0 && myoTimer < myoCountConstant) {
+            if (bulletCount < 10)
+            {
+                Shoot ();                    
+            }
+            bulletCount = bulletCount + 1;
+            //}
+            ExtendUnlockAndNotifyUserAction (thalmicMyo);
+		} 
+        _lastPose = thalmicMyo.pose;
+
+        if (myoTimer > myoCountConstant && thalmicMyo.pose != Pose.Fist){
+            myoTimer = 0f;
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime){
+            DisableEffects();
+            bulletCount = bulletCount - 1;
+            if (bulletCount < 0) bulletCount = 0;
+        }
+
+
+    }
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction (ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard) {
+            myo.Unlock (UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction ();
+    }
+
+    public void DisableEffects ()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+        timer = 0f;
+
+        gunAudio.Play ();
+
+        gunLight.enabled = true;
+
+        gunParticles.Stop ();
+        gunParticles.Play ();
+
+        gunLine.enabled = true;
+        gunLine.SetPosition (0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+            if(enemyHealth != null)
+            {
+                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+            }
+            gunLine.SetPosition (1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
